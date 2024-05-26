74d1a6da2c734afc8bcf5b5042cb5044747c26d8
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-05-26 15:26:28 -0400

    Agrega la página About junto con su estructura inicial

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <>
+        <h1>Nostos</h1>
+        <h3>Página en la cual se debe mostrar la información sobre de la empresa</h3>
+    </>
+  )
+}
