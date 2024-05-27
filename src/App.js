690191cd3543b467ff08ebbaf5d97611d58b406a
Matidmartinez <martinez.dmatias@gmail.com>
2024-05-26 21:50:07 -0400

    Se separa footer en su respectivo archivo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Parcels} from './pages/Parcels';
 import {About} from './pages/About';
 import {FAQ} from './pages/FAQ'
 import {Contact} from './pages/Contact'
+import {Footer} from './componentes/Footer'
 
 // Componentes para las secciones de la aplicación
 
@@ -19,25 +20,6 @@ const Title = ({ text }) => {
   );
 };
 
-
-// Componente Footer
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="footer-logo">
-        <img src="https://i.imgur.com/6sT02j6.png" alt="Terrasol Logo" />
-      </div>
-      <div className="footer-address">
-        Dirección: Entre Talca y la Región de Los Lagos
-      </div>
-      <div className="footer-contact">
-        <a href="https://wa.me/56950186583" target="_blank" rel="noopener noreferrer">
-          <a></a> +56 9 50186583
-        </a>
-      </div>
-    </footer>
-  );
-};
 
 // Componente principal App
 const App = () => {
---
+++ src/componentes/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+    <div className="footer-logo">
+      <img src="https://i.imgur.com/6sT02j6.png" alt="Terrasol Logo" />
+    </div>
+    <div className="footer-address">
+      Dirección: Entre Talca y la Región de Los Lagos
+    </div>
+    <div className="footer-contact">
+      <a href="https://wa.me/56950186583" target="_blank" rel="noopener noreferrer">
+        <a></a> +56 9 50186583
+      </a>
+    </div>
+  </footer>
+  )
+}
