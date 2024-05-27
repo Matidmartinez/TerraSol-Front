9b4a3e63bc74581f107354cda1b55eda78a6bc7c
Matidmartinez <martinez.dmatias@gmail.com>
2024-05-26 23:14:09 -0400

    Se solucionan conflictos

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -1,20 +1,9 @@
 import React from 'react';
-
-<<<<<<< HEAD
-export function Card() {
-=======
 export function Card({titulo,descripcion}) {
->>>>>>> 2efb939417b341de17bb242a47ddbe2d1364bc02
     return (
         <div>
           <h1>{titulo}</h1>
             <p>{descripcion}</p>
         </div>
-<<<<<<< HEAD
-    );
-}
-
-=======
     )
 }
->>>>>>> 2efb939417b341de17bb242a47ddbe2d1364bc02
