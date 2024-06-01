4c61ba895c7e2253d864112f6e93011e6ce48ad1
Searriagada <sebastian.arriag93@gmail.com>
2024-06-01 11:19:35 -0400

    Se crea da formato a card, muesta imagen rectangular con fondo negro

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -1,9 +1,21 @@
 import React from 'react';
-export function Card({titulo,descripcion}) {
+
+export function Card({ id, titulo, region, descripcion, imagen }) {
     return (
-        <div>
-          <h1>{titulo}</h1>
-            <p>{descripcion}</p>
+        <div key={id} className='box'>
+            <div className='card'>
+                <div className='image'>
+                    <img src={imagen} alt='' />
+                    <div className='description'>
+                        <h1>{titulo}</h1>
+                        <h3>{region}</h3>
+                    </div>
+                </div>
+                <div className='overlay'>
+                    <h1>{titulo}</h1>
+                    <p>{descripcion}</p>
+                </div>
+            </div>
         </div>
     )
 }
