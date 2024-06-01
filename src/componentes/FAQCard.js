3792b55862801f174423a041f4166af464d2735e
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-06-01 03:05:55 -0400

    Àgrega botón de menos

--- src/componentes/FAQCard.js
+++ src/componentes/FAQCard.js
@@ -1,5 +1,6 @@
 import '../styles/FAQCard.css'
-import {ReactComponent as CircleButton} from '../assets/plus-button.svg'
+import { ReactComponent as CircleButtonPlus } from '../assets/plus-button.svg'
+import { ReactComponent as CircleButtonMinus} from '../assets/minus-button.svg'
 
 export const FAQCard = ({ qa, selected, toggle }) => {
 
@@ -7,7 +8,12 @@ export const FAQCard = ({ qa, selected, toggle }) => {
         <div className='FAQCardContainer' key={qa.id}>
         <div className='questionContainer' onClick={() => toggle(qa.id)}>
             <h3 className='question'>{qa.question}</h3>
-            <CircleButton className='circleButton' />
+            {selected === qa.id ? (
+                    <CircleButtonMinus className='circleButton' />
+                ) : (
+                    <CircleButtonPlus className='circleButton' />
+                )
+            }
         </div>
         <div className={selected === qa.id ? 'answerContainer show' : 'answerContainer'}>
             <p className='answer'>{qa.answer}</p>
