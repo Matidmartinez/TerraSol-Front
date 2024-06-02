b8a85360f2cbfbfb11f098f7452395804ed82753
Matidmartinez <martinez.dmatias@gmail.com>
2024-06-02 00:57:58 -0400

    Se agrega Vision

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,14 +16,26 @@ export const About = () => {
       <img src={fondoNosotros} alt="fondoNosotros" className="FondoUs" />
       <section className="section">
         <Title text="NUESTRA MISIÓN" />
-        <p>En <span className='negrita'>Terrasol</span>, nuestra misión es ofrecer parcelas únicas en el sur de Chile, combinando la belleza natural
-          de la región con un servicio excepcional. Nos dedicamos a crear comunidades sostenibles y armoniosas,
+        <p>En <span className='negrita'>Terrasol</span>, nuestra misión es ofrecer parcelas únicas en el sur de Chile, combinando <span className='negrita'>la belleza natural
+          de la región con un servicio excepcional</span>. Nos dedicamos a crear comunidades sostenibles y armoniosas,
           donde nuestros clientes puedan construir sus sueños y disfrutar de un entorno natural prístino.</p>
         <br></br>
         <p>Valoramos la conservación del medio ambiente y el desarrollo responsable, asegurando que cada parcela no
-          solo sea una inversión en tierra, sino también en calidad de vida. Nos comprometemos a brindar asesoramiento
-          personalizado y soluciones integrales que satisfagan las necesidades y aspiraciones de nuestros clientes,
+          solo sea una inversión en tierra, sino también en calidad de vida. Nos comprometemos a<span className='negrita'> brindar asesoramiento
+            personalizado</span> y soluciones integrales que satisfagan las necesidades y aspiraciones de nuestros clientes,
           mientras contribuimos al crecimiento y bienestar de las comunidades locales.</p>
+      </section>
+      <br></br>
+      <img src={fondoNosotros} alt="fondoNosotros" className="FondoUs" />
+      <section className="section">
+        <Title text="NUESTRA VISIÓN" />
+        <p>Aspiramos a ser líderes en la venta de parcelas en el sur de Chile, reconocidos por nuestra integridad, calidad y compromiso
+          con el medio ambiente.</p>
+        <br></br>
+        <p>
+          Nos vemos como impulsores del desarrollo responsable, donde la armonía entre el hombre y la naturaleza sea la norma. Nuestro objetivo 
+          es crear espacios que inspiren a nuestros clientes a<span className='negrita'> vivir vidas plenas y conectadas con el entorno</span>, mientras 
+          fomentamos un impacto positivo y duradero en las comunidades locales y en la preservación de la biodiversidad de la región.</p>
       </section>
       <br></br>
       <img src={fondoNosotros} alt="fondoNosotros" className="FondoUs" />
