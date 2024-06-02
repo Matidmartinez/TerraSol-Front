4d549b81b92d2049edc07ad41ef8a97e1a183a18
Matidmartinez <martinez.dmatias@gmail.com>
2024-06-02 00:51:49 -0400

    Se agrega titulo y se ajusta imagen, se agregan parragos y se separa por seccion

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,13 +16,13 @@ export const About = () => {
       <img src={fondoNosotros} alt="fondoNosotros" className="FondoUs" />
       <section className="section">
         <Title text="NUESTRA MISIÓN" />
-        <p>En <span className='negrita'>Terrasol</span>, nuestra misión es ofrecer parcelas únicas en el sur de Chile, combinando la belleza natural
-          de la región con un servicio excepcional. Nos dedicamos a crear comunidades sostenibles y armoniosas,
+        <p>En <span className='negrita'>Terrasol</span>, nuestra misión es ofrecer parcelas únicas en el sur de Chile, combinando <span className='negrita'>la belleza natural
+          de la región con un servicio excepcional</span>. Nos dedicamos a crear comunidades sostenibles y armoniosas,
           donde nuestros clientes puedan construir sus sueños y disfrutar de un entorno natural prístino.</p>
         <br></br>
-        <p>Valoramos la conservación del medio ambiente y el desarrollo responsable, asegurando que cada parcela no
-          solo sea una inversión en tierra, sino también en calidad de vida. Nos comprometemos a brindar asesoramiento
-          personalizado y soluciones integrales que satisfagan las necesidades y aspiraciones de nuestros clientes,
+        <p>Valoramos la<span className='negrita'> conservación del medio ambiente</span> y el desarrollo responsable, asegurando que cada parcela no
+          solo sea una inversión en tierra, sino también en calidad de vida. Nos comprometemos a brindar<span className='negrita'> asesoramiento
+          personalizado</span> y soluciones integrales que satisfagan las necesidades y aspiraciones de nuestros clientes,
           mientras contribuimos al crecimiento y bienestar de las comunidades locales.</p>
       </section>
       <br></br>
