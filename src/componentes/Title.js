96cafb25d496b8c921a1767b065503472137adb6
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-06-01 23:07:35 -0400

    Se crea el componente de título

--- src/componentes/Title.js
+++ src/componentes/Title.js
@@ -1,9 +1,12 @@
 import React from 'react'
+// Supports weights 400-900
+import '../styles/Title.css'
+import '@fontsource-variable/playfair-display';
 
 export const Title = ({text}) => {
   return (
-    <div className="title">
-      <h2>{text}</h2>
+    <div className="titleContainer">
+      <h1 className='title'>{text}</h1>
     </div>
   )
 }
