b7f926579b02bf5199a1c1a8c0d0798191292a09
Searriagada <sebastian.arriag93@gmail.com>
2024-06-02 21:01:00 -0400

    Se corrige error en home y parcels con componente card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Card } from '../componentes/Card';
 import { Carousel } from '../componentes/Carousel';
 import { Title } from '../componentes/Title';
@@ -7,56 +8,58 @@ import '../styles/Carousel.css'
 import '../styles/Header.css'
 import reviewData from '../data/reviewData.json';
 
-//Llamando a la API y guardando los datos en u arreglo, los 3 primeros.
-let cards = [];
-fetch('http://localhost/terrasol-backend/controller/ParcelController.php')
-  .then((response) => response.json())
-  .then((data) => {
-    cards = data.slice(0, 3);
-    console.log(cards);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
 
 export const Home = () => {
-  return (
-    <>
-      <section className="HeaderSection">
-        <Header 
-          text = 'Ven y descubre la parcela de tus sueños'
-        />
-      </section>
+  const [cards, setCards] = useState([]);
 
-      <section className="NewParcel">
-        <Title
-          text='Conoce nuestros nuevos proyectos'
-        />
-        <div className='cardContainer'>
-          {
-            cards.map((card) => (
-              <Card
-                id={card.id_parcela}
-                titulo={card.nombre_parcela}
-                region={card.region_parcela}
-                descripcion={card.descripcion_parcela}
-                metros = {card.metros_2}
-                precio = {card.precios_desde}
-                imagen={card.img_1}
-              />
-            ))
-          }
-        </div>
-      </section>
-      <section className='reviewSection'>
-        <Title
-          text='Reseñas'
-        />
-        <div className='carouselSection'>
-          <Carousel data={reviewData.slides} />
-        </div>
-      </section>
+  useEffect(() => {
+    fetch('http://localhost/terrasol-backend/controller/ParcelController.php')
+      .then((response) => response.json())
+      .then((data) => {
+        setCards(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }, []);
 
-    </>
-  )
-}
+    return (
+      <>
+        <section className="HeaderSection">
+          <Header
+            text='Ven y descubre la parcela de tus sueños'
+          />
+        </section>
+
+        <section className="NewParcel">
+          <Title
+            text='Conoce nuestros nuevos proyectos'
+          />
+          <div className='cardContainer'>
+            {
+              [cards.slice(0, 3).map((card) => (
+                <Card
+                  id={card.id_parcela}
+                  titulo={card.nombre_parcela}
+                  region={card.region_parcela}
+                  descripcion={card.descripcion_parcela}
+                  metros={card.metros_2}
+                  precio={card.precios_desde}
+                  imagen={card.img_1}
+                />
+              ))]
+            }
+          </div>
+        </section>
+        <section className='reviewSection'>
+          <Title
+            text='Reseñas'
+          />
+          <div className='carouselSection'>
+            <Carousel data={reviewData.slides} />
+          </div>
+        </section>
+
+      </>
+    )
+  }
--- src/pages/Parcels.js
+++ src/pages/Parcels.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from '../componentes/Card';
 import { Title } from '../componentes/Title';
 import '../styles/Card.css'
 
 //Llamando a la API y guardando los datos en u arreglo, los 3 primeros.
 let cards = [];
-fetch('http://localhost/terrasol-backend/controller/ParcelController.php')
-  .then((response) => response.json())
-  .then((data) => {
-    cards = data;
-    console.log(cards);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+
+
 
 export const Parcels = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost/terrasol-backend/controller/ParcelController.php')
+      .then((response) => response.json())
+      .then((data) => {
+        setCards(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <>
       <section className='AllParcels'>
@@ -31,8 +37,8 @@ export const Parcels = () => {
                 titulo={card.nombre_parcela}
                 region={card.region_parcela}
                 descripcion={card.descripcion_parcela}
-                metros = {card.metros_2}
-                precio = {card.precios_desde}
+                metros={card.metros_2}
+                precio={card.precios_desde}
                 imagen={card.img_1}
               />
             ))
