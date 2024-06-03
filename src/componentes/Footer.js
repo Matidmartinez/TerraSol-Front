6bbfb38c7f100edaa24ff0fccdeb96f5ff6b2f8f
Matidmartinez <martinez.dmatias@gmail.com>
2024-06-02 21:08:32 -0400

    Se agregan enlaces a iconos de redes sociales

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,31 +1,42 @@
-import React from 'react'
-import logo from '../assets/logo.png'
-import logofb from '../assets/Facebook icon.png'
-import logoig from '../assets/Instagram icon.png'
-import '../styles/Footer.css'
+import React from 'react';
+import logo from '../assets/logo.png';
+import logofb from '../assets/Facebook icon.png';
+import logoig from '../assets/Instagram icon.png';
+import '../styles/Footer.css';
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-logo">
-        <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo de Terrasol" /> 
       </div>
       <div className="footer-address">
         Dirección: Entre Talca y la Región de Los Lagos
       </div>
       <div className="footer-contact">
         <a href="https://wa.me/56950186583" target="_blank" rel="noopener noreferrer">
-          <a></a> +56 9 50186583
+          <i className="fab fa-whatsapp"></i> +56 9 50186583 
         </a>
       </div>
-      <div class="footer-logos">
-        <div class="fb-logo-footer">
-          <img src={logofb} alt="Logofb" />
-        </div>
-        <div class="ig-logo-footer">
-          <img src={logoig} alt="Logoig" />
-        </div>
+      <div className="footer-logos">
+        <a 
+          href="https://www.facebook.com/tuparcelaenelsurdechile?mibextid=ZbWKwL" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          aria-label="Facebook de Terrasol"
+        >
+          <img src={logofb} alt="Facebook" />
+        </a>
+        <a 
+          href="https://www.instagram.com/terrasol_parcelas/?igsh=eG51a2w4NGx5ODJu" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          aria-label="Instagram de Terrasol"
+        >
+          <img src={logoig} alt="Instagram" />
+        </a>
       </div>
     </footer>
-  )
-}
+  );
+};
+
