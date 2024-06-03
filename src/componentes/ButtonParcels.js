53f3bb7d76b770fa5cc543857da9c4a6d62a5529
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-06-03 15:01:13 -0400

    Se refactoriza código del botón cotizar, eliminando así los warnings

--- src/componentes/ButtonParcels.js
+++ src/componentes/ButtonParcels.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { NavLink, Routes, Route } from 'react-router-dom';
 import '../styles/ButtonCoti.css'
-import { Contact } from '../pages/Parcels.js';
+import { Parcels } from '../pages/Parcels';
 
 export function ParcelsButton() {
     return (
-        <NavLink to="/contacto" className={({ isActive }) => isActive ? "parcels-button active" : "parcels-button"}>
+        <>
+         <NavLink to="/contacto" className={({ isActive }) => isActive ? "parcels-button active" : "parcels-button"}>
             Ver Parcelas
         </NavLink>
-    );
-}
-
-function App() {
-    return (
         <div>
-            <Routes>
-                <Route path="/parcelas" element={<Parcels />} />
-            </Routes>
+            
+        <Routes>
+            <Route path="/parcelas" element={<Parcels />} />
+        </Routes>
         </div>
+        </>
     );
 }
